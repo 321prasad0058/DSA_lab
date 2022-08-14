b4eaa94aefbc4f0cab1ba3d9a07e24ce70bc1d6f
321prasad0058 <110602321+321prasad0058@users.noreply.github.com>
2022-08-14 11:22:25 +0530

    Update quadratic.java

--- quadratic.java
+++ quadratic.java
@@ -1,10 +1,11 @@
-public class quadratic{
+public class quad
+{
 	public static void main(String[]args){
     int a=1;
     int b=-7;
     int c=10;
     double x1,x2,D;
-
+    System.out.println("Quadratic Roots");
     D=(b*b)-4*a*c;
     x1=(-b+Math.sqrt(D))/2*a;
     x2=(-b-Math.sqrt(D))/2*a;
