54ae7ad46d1a9dae23edb94ee390109e19699e8a
321prasad0058 <110602321+321prasad0058@users.noreply.github.com>
2022-08-14 11:18:45 +0530

    Update bikers.java

--- bikers.java
+++ bikers.java
@@ -1,5 +1,7 @@
-public class bikers{
-	public static void main(String[]args){
+public class bikers1
+{
+	public static void main(String[]args)
+	{
     int b=5;
     int s1=100,s2=120,s3=140,s4=160,s5=120;
     int avg;
@@ -7,15 +9,15 @@ public class bikers{
     avg=(s1+s2+s3+s4+s5)/5;
 
     if(s1>avg)
-        System.out.println("Biker 1 qualified ");
+        System.out.println("Biker 1 is qualified ");
     if(s2>avg)
-        System.out.println("Biker 2 qualified ");
+        System.out.println("Biker 2 is qualified ");
     if(s3>avg)
-        System.out.println("Biker 3 qualified "); 
+        System.out.println("Biker 3 is qualified "); 
     if(s4>avg)
-        System.out.println("Biker 4 qualified ");
+        System.out.println("Biker 4 is qualified ");
     if(s5>avg)
-        System.out.println("Biker 5 qualified ");   
+        System.out.println("Biker 5 is qualified ");   
    
-    }
+      }
 }
